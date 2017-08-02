8703820a6c48e3c1f2de95747bc19fe9ebb8f72c
Jakub Pavelek <jakub.pavelek@linaro.org>
2017-08-02 20:34:52 +0200

    Initial Dropbox tutorial integrated for testing.

--- app/src/main/java/com/afte9/dropboxplayer/Database.java
+++ app/src/main/java/com/afte9/dropboxplayer/Database.java
@@ -1,9 +1,17 @@
 package com.afte9.dropboxplayer;
 
+import java.io.IOException;
 
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.ListFolderResult;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.users.FullAccount;
 
 class Database {
     private static final Database ourInstance = new Database();
+    private static final String DBX_ACCESS_TOKEN = "gn_DmlOdsXwAAAAAAAAADAm6YDXZGBg6EWb--MzKQtWGWTI74XI1bdpONsNZE7Va";
 
     static Database getInstance() {
         return ourInstance;
@@ -27,5 +35,31 @@ class Database {
 
     protected String[] getPlaylists() {
         return db;
+    }
+
+    public void linkDropbox() throws DbxException, IOException {
+        // Create Dropbox client
+        DbxRequestConfig config = new DbxRequestConfig("digitalnoise/0.1");
+        DbxClientV2 client = new DbxClientV2(config, DBX_ACCESS_TOKEN);
+
+        // Get current account info
+        FullAccount account = client.users().getCurrentAccount();
+        System.out.println(account.getName().getDisplayName());
+
+        // Get files and folder metadata from Dropbox root directory
+        ListFolderResult result = client.files().listFolder("");
+        while (true) {
+            for (Metadata metadata : result.getEntries()) {
+                System.out.println(metadata.getPathLower());
+            }
+
+            if (!result.getHasMore()) {
+                break;
+            }
+
+            result = client.files().listFolderContinue(result.getCursor());
+        }
+
+
     }
 }
--- app/src/main/java/com/afte9/dropboxplayer/Splash.java
+++ app/src/main/java/com/afte9/dropboxplayer/Splash.java
@@ -2,18 +2,40 @@ package com.afte9.dropboxplayer;
 
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class Splash extends AppCompatActivity {
+import com.dropbox.core.DbxException;
+import com.dropbox.core.android.AuthActivity;
+
+import java.io.IOException;
+
+public class Splash extends AuthActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+
+        //FIXME - this is a hack - we should not do networking / long running operations in UI thread but spin new thread instead
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
         Database db = Database.getInstance();
         db.readFiles();
+        try {
+            db.linkDropbox();
+        } catch (DbxException e){
+            Log.d("DigiNoise", "Dropbox exception" + e);
+            //TODO - handle Dropbox exception
+        } catch (IOException e) {
+            Log.d("DigiNoise", "IOException" + e);
+            //TODO - handle IO exception
+        }
 
         CountDownTimer t = new CountDownTimer(1000, 1000) {
             @Override
